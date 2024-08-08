e61954a352fd45d6569fe48b83526301d7461cf1
justenjoy <prohor.ruslan068@gmail.com>
2024-08-08 15:07:25 +0200

    Finished for the moment

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-/*Creating a 16x16 grid div */
-
 // creating the div
 const div1 = document.createElement(`div`);
 
-//giving them ids
-div1.id = `container1`
+//giving them classes
+div1.classList.add = `grid-item`
 
 // appending them to the DOM
 const containerbigdaddy = document.querySelector(`#container`)
@@ -12,33 +10,51 @@ containerbigdaddy.appendChild(div1);
 
 // Function for creating more divs
 function createdivs  () {for(i = 1; i < 320; i++)
-    {const div1 = document.createElement(`div`);containerbigdaddy.appendChild(div1);div1.id = `container1`}}
-createdivs ();
-// Selecting all divs
-const alldivs = document.querySelectorAll(`#container > div`);
+    {const div1 = document.createElement(`div`);
+        div1.classList.add(`grid-item`)
+        containerbigdaddy.appendChild(div1);
+         
+    }}
+    createdivs  ()
+// Changing dinamically the color
+function randomizeColor () {  let color = 0xFFFFFF
+    const random = Math.floor(Math.random()* color) 
+    let randColor = random.toString(16)
+    let finalColor = randColor.padStart(6, `0`);
+return `#${finalColor.toUpperCase()}`}
+
 
 // Function for creating hover effect
-alldivs.forEach(div1 => {div1.addEventListener(`mouseover`,( ) => {div1.classList.add(`special`)})})
-
-
-
-/*function createhovereffect () {div1.addEventListener(`mouseover`,() => {
-    for (i = 0 ; i <= alldivs.length; i++) {
-        
-        alldivs[i].setAttribute
-        ("style","background-color: red")}
-    
-})}
-createhovereffect();*/
-
+function hovereffect () { const alldivs = document.querySelectorAll(`#container > .grid-item`);
+    alldivs.forEach(div1 => {div1.addEventListener(`mouseover`,( ) => {div1.style.backgroundColor = randomizeColor ()})})}
+hovereffect ()
 
 // Adding the button that will generate a div grid
-
 const buttonselector = document.querySelector(`#button1`)
-const prompt1 = prompt("choose between 0-100 squares","")
-function creategridonclick () {buttonselector.addEventListener(`click`, 
-    ( ) => {prompt1})
-if (prompt1 >= 0 && prompt1 <= 100 ) {for(i = 1; i < prompt1; i++)
-    {const div1 = document.createElement(`div`);containerbigdaddy.appendChild(div1);div1.id = `container1`}}
+function creategridonclick() {
+    buttonselector.addEventListener('click', () => {
+        const prompt1 = parseInt(prompt("Choose between 0-100 squares", ""), 10);
+        if (!isNaN(prompt1) && prompt1 >= 0 && prompt1 <= 100) {
+            containerbigdaddy.innerHTML = ''; // Clear existing grid
+            
+            // Create new grid based on user input
+            for (let i = 1; i <= prompt1 * prompt1; i++) {
+                const div1 = document.createElement('div');
+                div1.className = 'grid-item'; // Use a class instead of an ID
+                containerbigdaddy.appendChild(div1);
+            }
+            
+            // Reapply hover effect to new grid items
+            hovereffect();
+        } else {
+            alert("Please enter a valid number between 0 and 100.");
+        }
+    });
 }
     creategridonclick () 
+    hovereffect();
+
+    
+
+   
+
